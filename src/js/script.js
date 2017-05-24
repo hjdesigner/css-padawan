faf9e473055d20d2e48d3e61500b869f46907c3b
Henrique Rodrigues <hjdesigner23@gmail.com>
2017-05-24 00:21:50 -0300

    Added sharing bar

--- src/js/script.js
+++ src/js/script.js
@@ -117,3 +117,10 @@ function respond(val) {
     document.querySelector('.spoken-response__text').innerHTML = val;
 
 }
+(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.9&appId=254031111302534";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
